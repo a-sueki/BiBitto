bbf8d4fdeaf2fcf68e900bdee818e9caaeb25dbb
a-sueki <sueki.aoi@gmail.com>
2017-10-25 12:02:16 +0900

    [x] レイアウト調整(10/25)

--- BiBitto/HomeViewController.swift
+++ BiBitto/HomeViewController.swift
@@ -30,11 +30,11 @@ class HomeViewController: UIViewController {
         print(view.frame)
         view.backgroundColor = UIColor.gray
         let titleLabel: TTTAttributedLabel = TTTAttributedLabel(frame: CGRect(
-            x: view.frame.width/20 * 8, //y
-            y: view.frame.height/10 * 7,//x
-            width: view.frame.height/2 , //h
-            height: view.frame.width/10 * 1)) //w
-        titleLabel.backgroundColor = UIColor.red
+            x: view.frame.width/20 * 8,
+            y: view.frame.height/10 * 5,
+            width: view.frame.height/2 ,
+            height: view.frame.width/10 * 1))
+        titleLabel.backgroundColor = UIColor.white
         view.addSubview(titleLabel)
         print(titleLabel.frame)
 
@@ -42,11 +42,13 @@ class HomeViewController: UIViewController {
         titleLabel.numberOfLines = 0
         titleLabel.font = UIFont.systemFont(ofSize: 24)
         titleLabel.verticalAlignment = .top
+        
         // ラベルを90°回転させる
         let angle = Double.pi/2
         titleLabel.transform = CGAffineTransform(rotationAngle: CGFloat(angle))
         
-        let title = "行動力を生む心がけ"
+        //let title = "行動力を生む心がけ"
+        let title = "迷わない生き方"
         titleLabel.setText(title) { (mutableAttributedString) -> NSMutableAttributedString! in
             mutableAttributedString?.addAttribute(NSAttributedStringKey(rawValue: kCTVerticalFormsAttributeName as String as String), value: true, range: NSMakeRange(0,(mutableAttributedString?.length)!))
             return mutableAttributedString
@@ -55,22 +57,25 @@ class HomeViewController: UIViewController {
         // 縦書き対応(本文)
         print(view.frame)
         let textLabel: TTTAttributedLabel = TTTAttributedLabel(frame: CGRect(
-            x: view.frame.width/10,
-            y: view.frame.height/20 * 3,
-            width: view.frame.width/5 * 2,
-            height: view.frame.height/2))
-        textLabel.backgroundColor = UIColor.yellow
+            x: view.frame.width/100 * 7,
+            y: view.frame.height/10 * 5,
+            width: view.frame.height/2,
+            height: view.frame.width/2))
+        textLabel.backgroundColor = UIColor.white
         view.addSubview(textLabel)
         print(textLabel.frame)
 
         textLabel.textColor = UIColor.black
         textLabel.numberOfLines = 0
         textLabel.font = UIFont.systemFont(ofSize: 16)
-        textLabel.verticalAlignment = .top
+        textLabel.verticalAlignment = .center
+        
         // ラベルを90°回転させる
         textLabel.transform = CGAffineTransform(rotationAngle: CGFloat(angle))
         
-        let text = "国境の長いトンネルを抜けると雪国であった。夜の底が白くなった。信号所に汽車が止まった。\n向側の座席から娘が立って来て、島村の前のガラス窓を落した。雪の冷気が流れこんだ。娘は窓いっぱいに乗り出して、遠くへ叫ぶように、\n「駅長さあん、駅長さあん。」\n明りをさげてゆっくり雪を踏んで来た男は、襟巻で鼻の上まで包み、耳に帽子の毛皮を垂れていた。qaqqqqqqqqqqqあああああああああああああああああああああ"
+        //let text = "日頃から威張っている人ほど、\nいざっていうときになると黙りこんでしまいます。\n\n日頃から「やる」って言いふらしている人ほど、\nいざっていうときになるとなにもやらないものです。\n\n未知なることを知ろうとすること。本質を見抜こうとすること。その意識が一番、行動につながります。"
+        //let text = "日頃から威張っている人ほど、\nいざっていうときになると黙りこんでしまいます。"
+        let text = "最もつまらないと思うのは\n人との約束を破る人ではなく、\n自分との約束を破る人です。"
         textLabel.setText(text) { (mutableAttributedString) -> NSMutableAttributedString! in
             mutableAttributedString?.addAttribute(NSAttributedStringKey(rawValue: kCTVerticalFormsAttributeName as String as String), value: true, range: NSMakeRange(0,(mutableAttributedString?.length)!))
             return mutableAttributedString
